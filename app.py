5c24cf40e876280bc23342415a33a6d7e1242fc0
justgyan2025 <justgyan2025@gmail.com>
2025-04-05 14:51:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -89,6 +89,19 @@ def nps():
 def others():
     return render_template('others.html', firebase_config=firebase_config)
 
+@app.route('/nps_vatsalya')
+@login_required
+def nps_vatsalya():
+    firebase_config = {
+        'apiKey': os.getenv('FIREBASE_API_KEY'),
+        'authDomain': os.getenv('FIREBASE_AUTH_DOMAIN'),
+        'projectId': os.getenv('FIREBASE_PROJECT_ID'),
+        'storageBucket': os.getenv('FIREBASE_STORAGE_BUCKET'),
+        'messagingSenderId': os.getenv('FIREBASE_MESSAGING_SENDER_ID'),
+        'appId': os.getenv('FIREBASE_APP_ID')
+    }
+    return render_template('nps_vatsalya.html', firebase_config=firebase_config)
+
 @app.route('/api/stock/<symbol>')
 def get_stock_details(symbol):
     try:
